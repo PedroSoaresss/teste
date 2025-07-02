4ed5e7fd050155ea0357ec9d3c2592e57f98b723
Pedro Soares <phenrique7579@gmail.com>
2025-07-02 17:10:42 -0300

    vesion total finnaly

--- Quiz-App/src/components/results.jsx
+++ Quiz-App/src/components/results.jsx
@@ -13,24 +13,27 @@ function Results({ questionsBank, optionsSelected, restartQuiz }) {
 
   const score = getScore();
   const questionComponent = () => {
-    if (score >= 7) {
-        return <p className="sucesso">Parabéns ! Você foi muito bem.</p>;}
-        else if (score <= 6){
-            return <p className="error"> Horrivél ! Tente novamente amigo.</p> 
-        } 
-  }
- 
+    if (score >= 8) {
+      return (
+        <div className="centralizar">
+          <img src="../src/assets/feliz.jpeg" />
+        </div>
+      );
+    } else if (score >= 6) {
+      return <img src="../src/assets/tentativa.jpeg" />;
+    } else if (score <= 5) {
+      return <img src="../src/assets/emoji triste.jpeg" />;
+    }
+  };
 
   return (
     <div>
       <h2>Quiz Completo !</h2>
       <p>
-        {questionComponent()}
-        {" "}
-        Sua Pontuação:{score}/{questionsBank.length}
+        {questionComponent()} Sua Pontuação:{score}/{questionsBank.length}
       </p>
       <button className="restart-button" onClick={restartQuiz}>
-       Recomeçar 
+        Recomeçar
       </button>
     </div>
   );
